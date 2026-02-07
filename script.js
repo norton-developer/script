f8bf4fa6cce6ced4d7c0cfd115160f8002518f93
norton-developer <karpa9595@gmail.com>
2026-02-08 02:40:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello")
+// script.js на GitHub
+
+// Можно добавить лог для проверки
+console.log("MODULE LOADED FROM DISK!");
+
+// Твой основной код.
+// НЕ НУЖНО оборачивать в Java.perform внутри script.js, если ты используешь require.
+// Но так как твой чит использует Interceptor и Java, лучше сделать так:
+
+function main() {
+    console.log("Starting cheat logic...");
+    
+    // СЮДА ВСТАВЛЯЙ ВЕСЬ ТВОЙ КОД ЧИТА
+    // (var validVTable..., waitForModule... и т.д.)
+    
+    var validVTable = null; 
+    var GIFT_TARGET = "cyberDance";
+    // ... и так далее ...
+    
+    // ... твой интервал waitForModule ...
+    var waitForMod = setInterval(function() {
+        // ...
+    }, 1000);
+}
+
+// Запускаем функцию сразу
+main();
